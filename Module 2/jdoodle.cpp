092b11668872481af2cdc14211b9e19351c4d6c9
tylersaephan24 <saephan2006@gmail.com>
2026-02-02 21:31:35 -0800

    Add files via upload

---
+++ Module 2/jdoodle.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main() {
+std::cout<<"Hello Inventory System!\n";
+std::cout<<"\tData Types:\n";
+std::cout<<"Size of int:"<<sizeof(int)<<"bytes\n";
+std::cout<<"Size of float:"<<sizeof(float)<<"bytes\n";
+std::cout<<"Size of char:"<<sizeof(char)<<"bytes\n";
+return 0;
+}
