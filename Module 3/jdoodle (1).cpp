0d07c87470025336cc8475477af7410b5a74b490
tylersaephan24 <saephan2006@gmail.com>
2026-02-02 22:47:10 -0800

    Add files via upload

---
+++ Module 3/jdoodle (1).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>  
+using namespace std;
+
+int main() {
+    string itemName;
+    int quantity; 
+    float cost;
+    
+    cout << "Enter item name: ";
+    cin >> itemName;
+    cout << "Enter quantity: ";
+    cin >> quantity;  
+    cout << "Enter cost: ";
+    cin >> cost;   
+    
+    cout << "Total cost for " << itemName << ": $" << (quantity * cost) << endl;  // Fixed std:: syntax
+    return 0;
+}
